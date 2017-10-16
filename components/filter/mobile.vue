bb63c8f3f789049dd27d4b91a48a0143b8a7e92a
Jungmin Ji <fdev.jm@gmail.com>
2017-10-16 09:20:17 +0900

    add filter btn options, and reset

--- components/filter/mobile.vue
+++ components/filter/mobile.vue
@@ -8,22 +8,22 @@
         v-icon fa-times
       v-layout(row wrap)
         v-flex(xs4 class="text-xs-right")
-          v-btn(flat class="option") 학교별
+          v-btn(flat class="option" @click="changeBtnStyle(0); byInstitution = true; byCategory = false; byProfessor = false") 학교별
         v-flex(xs4 class="text-xs-center")
-          v-btn(flat class="option") 연구별
+          v-btn(flat class="option" @click="changeBtnStyle(1); byInstitution = false; byCategory = true; byProfessor = false") 연구별
         v-flex(xs4 class="text-xs-left")
-          v-btn(flat class="option") 교수님
+          v-btn(flat class="option" @click="changeBtnStyle(2); byInstitution = false; byCategory = false; byProfessor = true") 교수님
 
-      v-select(label="학교명" :items="items.institution" v-if="byInstitution" v-model="model.institution")
-      v-select(label="학과명" v-if="byInstitution" v-model="model.department")
+      v-select(label="학교명" :items="items.institution" v-if="byInstitution" v-model="model.institution" :placeholder="placeholder.institution")
+      v-select(label="학과명" v-if="byInstitution" v-model="model.department" :placeholder="placeholder.department")
 
-      v-select(label="연구 대분류" :items="items.category" v-if="byCategory" v-model="model.category")
-      v-select(label="소분류" v-if="byCategory" v-model="model.subCategory")
+      v-select(label="연구 대분류" :items="items.category" v-if="byCategory" v-model="model.category" :placeholder="placeholder.category")
+      v-select(label="소분류" v-if="byCategory" v-model="model.subCategory" :placeholder="placeholder.subCategory")
 
-      v-select(label="교수님" :items="items.professor" v-if="byProfessor" v-model="model.professor")
+      v-select(label="교수님" :items="items.professor" v-if="byProfessor" v-model="model.professor" :placeholder="placeholder.professor")
 
       v-layout(row wrap)
-        v-btn
+        v-btn(@click="reset")
           v-icon refresh
           span 초기화
         v-btn(color="primary")
@@ -45,6 +45,13 @@ export default {
       subCategory: null,
       professor: null
     },
+    placeholder: {
+      institution: null,
+      department: null,
+      category: null,
+      subCategory: null,
+      professor: null
+    },
     isOpen: false,
     byInstitution: true,
     byCategory: false,
@@ -55,6 +62,47 @@ export default {
     this.$eventBus.$on('filter-mobile-fixed', this.filterFixed)
   },
   methods: {
+    changeBtnStyle (n) {
+      const btns = this.$sa('.option')
+      btns.forEach((btn) => {
+        btn.style.borderColor = 'transparent'
+      })
+      btns[n].style.borderColor = '#616161'
+    },
+    initValue () {
+      const query = this.$route.query
+      const model = this.model
+      const ph = this.placeholder
+      if (query.superCategory) {
+        this.byCategory = true
+        this.byInstitution = false
+        this.byProfessor = false
+        model.category = query.superCategory
+        ph.category = query.superCategory
+        if (query.category) {
+          model.subCategory = query.category
+          ph.subCategory = query.category
+        }
+      }
+      if (query.institution) {
+        this.byCategory = false
+        this.byInstitution = true
+        this.byProfessor = false
+        model.institution = query.institution
+        ph.institution = query.institution
+        if (query.department) {
+          model.department = query.department
+          ph.department = query.department
+        }
+      }
+      if (query.professor) {
+        this.byCategory = false
+        this.byInstitution = false
+        this.byProfessor = true
+        model.professor = query.professor
+        ph.professor = query.professor
+      }
+    },
     open () {
       const toolbar = this.$s('.toolbar')
       const filterContainer = this.$s('.filter-container')
@@ -67,6 +115,7 @@ export default {
         btns.forEach((btn) => {
           btn.style.borderRadius = '0'
           btn.style.color = '#616161'
+          btn.style.borderBottom = '2px solid transparent'
         })
         const query = this.$route.query
         const style = '2px solid #616161'
@@ -80,6 +129,7 @@ export default {
           btns[2].style.borderBottom = style
         }
       })
+      this.initValue()
     },
     close () {
       const filterContainer = this.$s('.filter-container')
@@ -101,6 +151,20 @@ export default {
       const filterContainer = this.$s('.filter-container')
       filterContainer.style.position = 'fixed'
       filterContainer.style.top = getComputedStyle(toolbar, null).getPropertyValue('height')
+    },
+    reset () {
+      const model = this.model
+      const ph = this.placeholder
+      for (let key in model) {
+        if (model.hasOwnProperty(key)) {
+          model[key] = null
+        }
+      }
+      for (let key in ph) {
+        if (ph.hasOwnProperty(key)) {
+          ph[key] = null
+        }
+      }
     }
   }
 }
